d7e3b437f7fb03beb17b98d2716aec7a66543f12
Pavlo Shevchenko <pshevchenko@gradle.com>
2024-01-26 15:52:40 +0100

    Enhance `@Translate` example

--- examples/src/main/java/io/github/pshevche/funnotations/examples/translate/TranslateExample.java
+++ examples/src/main/java/io/github/pshevche/funnotations/examples/translate/TranslateExample.java
@@ -9,10 +9,16 @@ public class TranslateExample {
 
     public static void main(String[] args) {
         var enterprise = new Spaceship("USS Enterprise");
-        enterprise.flyFast("Mars");
+        enterprise.flyFast("Mercury");
 
         var polishEnterprise = new StatekKosmiczny(enterprise);
-        polishEnterprise.lataćSzybko("Earth");
+        polishEnterprise.lataćSzybko("Wenus");
+
+        var russianEnterprise = new КосмическийКорабль(enterprise);
+        russianEnterprise.быстроЛетать("Земля");
+
+        var germanEnterprise = new Raumschiff(enterprise);
+        germanEnterprise.schnellFliegen("Mars");
     }
 
 }
